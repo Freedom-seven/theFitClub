1063d3b45f84ea2dfa9996c177f437a2364386f8
FREEDOM <freewhynane62@gmail.com>
2023-01-28 03:32:36 +0000

    add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { Join } from "./components/Join";
+import { Plans } from "./components/Plans";
+import { Programs } from "./components/Programs";
+import { Reason } from "./components/Reasons";
+import { Testimonials } from "./components/Testimonials";
 function App() {
   return (
     <div className="App">
-      <span style={{ color: "white" }}>Gym website landing page 🚀 </span>
+      <Hero />
+      <Programs />
+      <Reason />
+      <Plans />
+      <Testimonials />
+      <Join />
+      <Footer />
     </div>
   );
 }
